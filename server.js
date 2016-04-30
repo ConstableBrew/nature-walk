6bb27ee88e1e9b1cb8d408a3435936d25d5f5388
Michael Brewer <mikeb@steelhouse.com>
2016-04-30 13:03:44 -0700

    finished basic setup

---
+++ server.js
@@ -0,0 +1,25 @@
+var port = process.env.PORT || 3000;
+var http = require('http');
+var url  = require('url');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static')("./");
+
+var server = http.createServer(function(req, res) {
+	console.log(req.url);
+
+	var done = finalhandler(req, res);
+	var urlParts = url.parse(req.url);
+
+	switch (urlParts.path) {
+		case '/':
+			res.writeHead(301, {Location: '/nature-walk/'})
+			res.end();
+			break;
+		default:
+			serveStatic(req, res, done);
+	}
+})
+.listen(port);
+
+console.log('listening on port', port);
